8dc3b6e1223b6154521ea2b7d571f6faee5ca80d
pamori2425 <463377.clot@fje.edu>
2024-10-24 20:03:58 +0200

    Commit 1 del projecte loginteller

---
+++ loginteller.c
@@ -0,0 +1,20 @@
+/* loginteller.c
+ * Based on monousuar.c at http://www.binefa.net/gnu/gcc/processos/Informacio_d_usuari.html
+ * www.binefa.cat
+ * 20230920
+*/
+#include <stdio.h>
+#include <unistd.h>    // getlogin()
+#include <stdlib.h>    // exit()
+
+int main(){
+    char *szUserName;
+
+    if((szUserName = getlogin())==NULL){
+        perror("getlogin");
+        exit(EXIT_FAILURE);
+    }
+    printf("Your username at the system is : %s\n",szUserName);
+
+    return (0);
+}
